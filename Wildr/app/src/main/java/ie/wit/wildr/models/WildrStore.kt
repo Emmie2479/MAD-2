755ed5c73f19f0a0180ffc040a2bc62c4881aa63
Emmie2479 <60179669+Emmie2479@users.noreply.github.com>
2022-03-02 12:47:48 +0000

    init commit

--- Wildr/app/src/main/java/ie/wit/wildr/models/WildrStore.kt
+++
@@ -1,8 +0,0 @@
-package ie.wit.wildr.models
-
-interface WildrStore {
-    fun findAll(): List<WildrModel>
-    fun create(animal: WildrModel)
-    fun update(animal: WildrModel)
-    fun delete(animal: WildrModel)
-}
---
+++ Wildr/app/src/main/java/ie/wit/wildr/ui/home/WelcomeFragment.kt
@@ -0,0 +1,47 @@
+package ie.wit.wildr.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ie.wit.wildr.R
+//import ie.wit.wildr.databinding.FragmentHomeBinding
+import ie.wit.wildr.databinding.FragmentWelcomeBinding
+
+class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        //val button: Button = binding.buttonAddImage
+        val homeViewModel =
+            ViewModelProvider(this).get(WelcomeViewModel::class.java)
+
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
